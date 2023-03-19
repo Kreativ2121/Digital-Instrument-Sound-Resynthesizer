625e6e40a9c22c6d0217167783308280776a3007
Dominik Ciesiołkiewicz <cd44289@zut.edu.pl>
2023-03-19 15:44:05 +0100

    Dodano odczytywanie i zapisywanie pliku wave.

--- Projekt_MatLab/plik_testowy.m
+++ Projekt_MatLab/plik_testowy.m
@@ -1,3 +1,21 @@
 clc;
 clear;
 
+%% Odczytaj plik wave,wygraj go na głośnikach i zapisz wave wyjściowy
+[audioData,fs] = audioread("src/download/CantinaBand3.wav");
+soundsc(audioData,fs)
+audiowrite("output/CantinaBand3.wav",audioData,fs);
+
+%% Odczytywanie pliku frame-by-frame
+% Tworzenie odczytywacza
+fileReader = dsp.AudioFileReader("Filename","src/download/CantinaBand3.wav");
+% Tworzenie writera, który będzie odtwarzał dźwięk
+deviceWriter = audioDeviceWriter("SampleRate",fileReader.SampleRate);
+
+while ~isDone(fileReader)
+    audioData = fileReader();
+    deviceWriter(audioData);
+end
+
+release(fileReader)
+release(deviceWriter)

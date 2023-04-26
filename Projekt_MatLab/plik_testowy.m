c1dcbb5fe7ee1f24395fb32bc400ea9230fff082
Dominik Ciesiołkiewicz <cd44289@zut.edu.pl>
2023-04-26 21:48:51 +0200

    Added square2000.wav file.

--- Projekt_MatLab/plik_testowy.m
+++ Projekt_MatLab/plik_testowy.m
@@ -3,7 +3,8 @@ clear;
 
 %% Odczytaj plik wave,wygraj go na głośnikach i zapisz wave wyjściowy
 % filetitle = "src/download/CantinaBand3.wav";
-filetitle = "src/records/KROSS/STRINGS_ACC_AMOL.wav";
+% filetitle = "src/records/kross/stereo/STRINGS_ACC_AMOL.wav";
+filetitle = "src/generated/mono/STRINGS_ACC_AMOL.wav";
 [audioData,fs] = audioread(filetitle);
 auInfo = audioinfo(filetitle);
 soundsc(audioData,fs)

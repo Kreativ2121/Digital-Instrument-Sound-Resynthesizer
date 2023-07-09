634490791cf4ac5b14dd590f1c2e133ac9910372
domin <domin@NZXT>
2023-07-10 00:52:41 +0200

    Spectral Peak Detection done in 50%.

--- Projekt_MatLab/new_synthesis.m
+++ Projekt_MatLab/new_synthesis.m
@@ -40,11 +40,72 @@ PointAmplitude = deg2rad(magnitude);
 MagnitudeDecibels = 20*log10(Ray);
 
 %%STEP 4
-MinimumPeakHeight = 3; %in dB
+MinimumPeakHeight = 15; %in dB
 FrequencyRangeLow = 20; %in hz
 FrequencyRangeHigh = 16000; %in hz
-AmplitudeRangeLow = 0; %in dB ?
-AmplitudeRangeHigh = 80; %in dB ?
+AmplitudeRangeLow = -70; %in dB ?
+AmplitudeRangeHigh = 0; %in dB ?
+
+counter = 1;
+FrequencyPeaks = [];
+
+% Find peaks
+% Loop on time frames
+% TODO Sprawić by krawędziowe elementy mogły być wliczane jako piki
+for j=1:size(MagnitudeDecibels,2)
+    % Loop on frequency bins
+    for i=2:size(MagnitudeDecibels,1)-1
+        if(MagnitudeDecibels(i,j) >= MagnitudeDecibels(i-1,j) && MagnitudeDecibels(i,j) >= MagnitudeDecibels(i+1,j))
+            FrequencyPeaks(1,counter) = MagnitudeDecibels(i,j);
+            FrequencyPeaks(2,counter) = i; % No. of bin
+            FrequencyPeaks(3,counter) = j; % No. of time frame
+            counter = counter + 1;
+        end
+    end
+end
+
+% Find valleys for all peaks
+PeakValleys = [];
+for i=1:size(FrequencyPeaks,2)
+    prevValue = FrequencyPeaks(1,i);
+    for j = FrequencyPeaks(2,i):-1:1
+        if(MagnitudeDecibels(j,FrequencyPeaks(3,i)) <= prevValue)
+            prevValue = MagnitudeDecibels(j,FrequencyPeaks(3,i));
+        else
+            PeakValleys(i,1) = prevValue;
+            break;
+        end
+    end
+
+    prevValue = FrequencyPeaks(1,i);
+    for j = FrequencyPeaks(2,i):size(MagnitudeDecibels,1)
+        if(MagnitudeDecibels(j,FrequencyPeaks(3,i)) <= prevValue)
+            prevValue = MagnitudeDecibels(j,FrequencyPeaks(3,i));
+        else
+            PeakValleys(i,2) = prevValue;
+            break;
+        end
+    end
+end
+PeakValleys = PeakValleys';
+
+% Assigning Peak Heights
+for i=1:size(FrequencyPeaks,2)
+    FrequencyPeaks(4,i) = FrequencyPeaks(1,i) - (PeakValleys(1,i) + PeakValleys(2,i))/2; % Peak heights in dB
+end
+
+% Filtering Small Peaks
+FrequencyPeaksHeightFiltered = [];
+counter = 1;
+for i=1:size(FrequencyPeaks,2)
+    if(FrequencyPeaks(4,i) >= MinimumPeakHeight)
+        FrequencyPeaksHeightFiltered(1,counter) = FrequencyPeaks(1,i);
+        FrequencyPeaksHeightFiltered(2,counter) = FrequencyPeaks(2,i);
+        FrequencyPeaksHeightFiltered(3,counter) = FrequencyPeaks(3,i);
+        FrequencyPeaksHeightFiltered(4,counter) = FrequencyPeaks(4,i);
+        counter = counter+1;
+    end
+end
 
 %%STEP 5
 

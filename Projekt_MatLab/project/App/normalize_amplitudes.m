953fbbb270bf4dbf4bb56d85a0e4875aa4f4b610
Kreativ2121 <dominikciesiolkiewicz@gmail.com>
2023-08-22 20:42:42 +0200

    Finished the app, added message boxes and fixed the tragic amplitude correction algorithm.

--- Projekt_MatLab/project/App/normalize_amplitudes.m
+++ Projekt_MatLab/project/App/normalize_amplitudes.m
@@ -2,11 +2,10 @@ function [peaks] = normalize_amplitudes(peaks, windowLength, beta)
 % normalize_amplitudes Function that normalizes the peak amplitudes in
 % regard to the windowLength. This version works only for Keiser window.
     % Normalization scale factor calculation for Kaiser window
-    W0_B = sum(kaiser(196,beta));
-    alpha = double(2/W0_B);
+    W0_B = sum(kaiser(windowLength,beta));
+    alpha = double(((windowLength)/128+1)/W0_B);
     
     % Normalized amplitude
-    N_Amp = peaks(11,:) * alpha;                    
-    peaks(11,:) = N_Amp;
+    peaks(11,:) = peaks(11,:) * alpha;
 end
 

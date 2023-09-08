63569cf9048b879eb7e6c12995f2300c7ded4b79
Kreativ2121 <dominikciesiolkiewicz@gmail.com>
2023-09-08 02:17:25 +0200

    Added yet another file to git.

--- Projekt_MatLab/project/MatLab/normalize_amplitudes.m
+++ Projekt_MatLab/project/MatLab/normalize_amplitudes.m
@@ -2,14 +2,9 @@ function [peaks] = normalize_amplitudes(peaks, windowLength, beta)
 % normalize_amplitudes Function that normalizes the peak amplitudes in
 % regard to the windowLength. This version works only for Keiser window.
     % Normalization scale factor calculation for Kaiser window
-    % W0_B = sum(kaiser(windowLength,beta));
-    % alpha = double(((windowLength)/128+1)/W0_B);
-    ks = kaiser(windowLength,beta);
-    ks = ks(floor(length(ks)/2):end);
-    W0_B = sum(ks);
-    % alpha = double(2/W0_B);
-    alpha = double(2/W0_B);
-    
+    W0_B = sum(kaiser(windowLength,beta));
+    alpha = double(5.05/W0_B);
+
     % Normalized amplitude              
     peaks(11,:) = peaks(11,:) * alpha;
 end
